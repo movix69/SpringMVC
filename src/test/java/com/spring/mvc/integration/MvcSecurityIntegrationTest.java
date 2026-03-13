50ff760446cdf5fcb23b5c23a23f678be1ae6cb3
Mananas <as@hotmail.es>
2026-03-13 13:14:32 +0100

    create db for test

--- src/test/java/com/spring/mvc/integration/MvcSecurityIntegrationTest.java
+++ src/test/java/com/spring/mvc/integration/MvcSecurityIntegrationTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -27,6 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class MvcSecurityIntegrationTest {
 
     @Autowired
