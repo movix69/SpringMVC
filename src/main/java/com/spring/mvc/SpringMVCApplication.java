33b2c00e5162ba287c3eae9a0a58790ffdafb8b0
Mananas <as@hotmail.es>
2026-03-13 13:06:42 +0100

    add main class pom

--- src/main/java/com/spring/mvc/SpringMVCApplication.java
+++ src/main/java/com/spring/mvc/SpringMVCApplication.java
@@ -10,7 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = "com.hibernate.repository")
 public class SpringMVCApplication {
 
-	static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(SpringMVCApplication.class, args);
 	}
 
